58f84e0a0dea5cc012402dd45cc1049df067e6ed
Denis Bondarchuk <denis@kaizen-design.ru>
2019-11-23 15:23:10 +0300

    Updates

---
+++ precache-manifest.f197f8a8593b45102e00831c83841973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cd00efdd7efff329f12ec26122e899",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "fa84fe7928a1b924dda5",
+    "url": "/drum-machine/static/css/main.0ed56915.chunk.css"
+  },
+  {
+    "revision": "7316614cf1be024a2a0b",
+    "url": "/drum-machine/static/js/2.a071c6d7.chunk.js"
+  },
+  {
+    "revision": "fa84fe7928a1b924dda5",
+    "url": "/drum-machine/static/js/main.6fb2aa93.chunk.js"
+  },
+  {
+    "revision": "1a35ca53cb0ec087786f",
+    "url": "/drum-machine/static/js/runtime-main.26c376bd.js"
+  }
+]);
