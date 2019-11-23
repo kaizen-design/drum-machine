91b304f5cc04b46197e854ddaed2f18695752860
Denis Bondarchuk <denis@kaizen-design.ru>
2019-11-23 15:21:52 +0300

    HTML layout created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,55 @@
 import React from 'react';
+import Navbar from './templates/layout/Navbar';
+import Footer from './templates/layout/Footer';
+
+/*const audioBank = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  }, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  }, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  }, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  }, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  }, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  }, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  }, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  }, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];*/
 
 class App extends React.Component {
   constructor(props) {
@@ -16,29 +67,57 @@ class App extends React.Component {
   render() {
     return (
       <div className="h-100 d-flex flex-column">
-        <nav className="navbar justify-content-center navbar-dark bg-gradient-dark shadow-sm">
-          <span className="navbar-brand mb-0 h1">Killer Drum Machine</span>
-        </nav>
-        <main role="main" className="App container-fluid h-100 pt-3 pt-lg-4 px-lg-4">
-
-        </main>
-        <footer className="text-center py-3">
-          <div className="container">
-            <small>
-              Designed & built by <a href="https://github.com/kaizen-design" target="_blank" rel="noopener noreferrer">Denis B.</a>
-            </small>
+        <Navbar brand={'Killer Drum Machine'} />
+        <main role="main" className="App container my-auto py-3">
+          <div id="drum-machine" className="row">
+            <div className="col-lg-8">
+              <div className="form-row">
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">Q</button>
+                </div>
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">W</button>
+                </div>
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">E</button>
+                </div>
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">A</button>
+                </div>
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">S</button>
+                </div>
+                <div className="col-4 mb-2">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">D</button>
+                </div>
+                <div className="col-4">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">Z</button>
+                </div>
+                <div className="col-4">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">X</button>
+                </div>
+                <div className="col-4">
+                  <button type="button" className="drum-pad btn btn-dark btn-block">C</button>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4">
+              <div className="jumbotron h-100 p-4 border">
+                <div className="form-group">
+                  <label htmlFor="volumeControl">Volume</label>
+                  <input type="range" className="custom-range" id="volumeControl" />
+                </div>
+                <div className="alert alert-dark text-center" role="alert">
+                  <b>Current Pad Title</b>
+                </div>
+              </div>
+            </div>
           </div>
-        </footer>
+        </main>
+        <Footer />
       </div>
     );
   }
 }
-
-/*const Title = (props) => {
-  return (
-    <h6 className="w-100 py-2 px-3 mb-0 bg-gradient-secondary text-white rounded-top shadow-sm"
-        dangerouslySetInnerHTML={{__html: props.title}} />
-  )
-};*/
 
 export default App;
